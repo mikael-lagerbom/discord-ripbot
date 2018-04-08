dcff371155ae4f2cd6aa674a7bc92ce707cec641
Mikael Lagerbom <tom.lagerbom@student.tut.fi>
2018-04-08 20:33:14 +0300

    added check for roll options

--- src/modules/roll.js
+++ src/modules/roll.js
@@ -1,8 +1,15 @@
 const roll = (message, messageWords) => {
-  const limit = parseInt(messageWords[1]);
-  if (!isNaN(limit)) {
-    const randInt = Math.floor(Math.random() * Math.floor(limit));
+  if (messageWords.length === 1) {
+    const randInt = Math.floor(Math.random() * Math.floor(100));
     message.channel.send(randInt);
+  } else if (messageWords.length === 2) {
+    const limit = parseInt(messageWords[1]);
+    if (!isNaN(limit)) {
+      const randInt = Math.floor(Math.random() * Math.floor(limit));
+      message.channel.send(randInt);
+    } else {
+      message.channel.send('yritä nyt');
+    }
   } else {
     message.channel.send('yritä nyt');
   }
