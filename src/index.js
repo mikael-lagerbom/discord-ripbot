496b4ee7a1d69b1110bc868a85c07d4d77fffe1c
Mikael Lagerbom <mikael.lagerbom@gofore.com>
2018-11-05 14:33:57 +0200

    added try-catch with timeout to login with max tries

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const client = new Discord.Client();
 
 const token = process.env.DISCORD_TOKEN;
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 const start = async () => {
   utils.migrateLatest(knex).then(async () => {
     const wisdoms = await knex('wisdoms').select('id');
@@ -35,8 +37,18 @@ client.on('message', message => {
   }
 });
 
-try {
-  client.login(token);
-} catch (err) {
-  throw err;
-}
+const login = async () => {
+  for (let tries = 0; ; tries++) {
+    try {
+      client.login(token);
+      break;
+    } catch (err) {
+      if (tries < 10) {
+        console.log('Login failed, sleeping');
+        sleep(30000);
+      } else throw err;
+    }
+  }
+};
+
+login();
