73b9e1fda519d180d1dbf5dee465f750cb85335b
Mikael Lagerbom <mikael.lagerbom@gofore.com>
2021-03-24 13:38:41 +0200

    heroku whot

--- src/knexfile.js
+++ src/knexfile.js
@@ -1,6 +1,6 @@
 module.exports = {
   client: 'pg',
-  connection: process.env.DATABASE_URL,
+  connection: process.env.DATABASE_URL + '?ssl=true',
   debug: false,
   ssl: { rejectUnauthorized: false }
 };
