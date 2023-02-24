11cbdd1eec2dc660fcce0a76f839f482b192ef70
Mikael Lagerbom <mikael.lagerbom@unity3d.com>
2023-02-24 16:36:52 +0200

    cleaned up roll

--- src/commands/roll.js
+++ src/commands/roll.js
@@ -16,24 +16,7 @@ module.exports = {
     ),
   async execute(interaction) {
     const max = interaction.options.getInteger('max') ?? 100;
+    const roll = Math.floor(generator.random_incl() * Math.floor(max || 100));
     await interaction.reply(`You rolled ${roll(max)}`);
   }
-};
-
-const roll = max => {
-  // if (messageWords.length === 1) {
-  //   const randInt = Math.floor(generator.random_incl() * Math.floor(100));
-  //   message.channel.send(randInt);
-  // } else if (messageWords.length === 2) {
-  //   const limit = parseInt(messageWords[1]);
-  //   if (!isNaN(limit)) {
-  //     const randInt = Math.floor(generator.random_incl() * Math.floor(limit));
-  //     message.channel.send(randInt);
-  //   } else {
-  //     message.channel.send('try pls');
-  //   }
-  // } else {
-  //   message.channel.send('try pls');
-  // }
-  return Math.floor(generator.random_incl() * Math.floor(max || 100));
 };
