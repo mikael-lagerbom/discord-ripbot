c73fecd58803dace8270068f88a3c90d33a0553d
Mikael Lagerbom <mikael.lagerbom@unity3d.com>
2023-03-11 16:31:41 +0200

    increased max length 5x

--- src/modules/openai.js
+++ src/modules/openai.js
@@ -7,7 +7,7 @@ const openai = new OpenAIApi(configuration);
 const model = 'gpt-3.5-turbo';
 
 const starter = {
-  role: 'system',
+  role: 'user',
   content:
     'You are a barely helpful, very sarcastic and passive aggressive assistant. Olet juuri ja juuri avulias, hyvin sarkastinen ja hieman passiivisaggressiivinen avustaja.'
 };
@@ -18,7 +18,7 @@ const askOpenAI = async prompt => {
   const response = await openai.createChatCompletion({
     model,
     messages: [starter, question],
-    max_tokens: 100
+    max_tokens: 500
   });
   return response.data.choices[0].message.content;
 };
