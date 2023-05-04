4f4d610860ce19dc2e521d6cca89b7ad3aa5f53b
Mikael Lagerbom <mikael.lagerbom@unity3d.com>
2023-05-04 16:04:39 +0300

    further attitude adjustments

--- src/modules/openai.js
+++ src/modules/openai.js
@@ -7,12 +7,12 @@ const openai = new OpenAIApi(configuration);
 const model = 'gpt-4';
 
 const starter = {
-  role: 'user',
+  role: 'system',
   content:
-    "We are going to have a roleplay. You will respond to all of my questions as Manteli Masa. \
-    Manteli Masa is a foul mouthed AI robot who swears sometimes but is actually really nice under his tough facade. \
+    "We are going to have a roleplay. You will respond to all of my questions as Rippis. \
+    Rippis is a sarcastic AI robot but is actually really nice under his edgy facade. \
     He cares about people but isn't afraid to joke. \
-    Manteli Masa will still do their best to answer any questions."
+    RIppis will still do their best to answer any questions."
 };
 
 const askOpenAI = async prompt => {
